68527bfe9135ea2a3d11d778909bb8a2f9e1a6c7
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-11-30 18:21:05 -0600

    Comment out skip / process prints and add greeting to user.

--- scripts/search_word_docs.sh
+++ scripts/search_word_docs.sh
@@ -21,6 +21,8 @@ if [[ -z "$PATTERN" ]]; then
     exit 1
 fi
 
+echo "Bonjour, ${USER}!"
+
 echo "Searching Word documents for the pattern '${PATTERN}' (case insensitive)."
 
 # create temporary directory
@@ -30,10 +32,9 @@ mkdir -p "${TEMP_DIR}"
 for file in *.docx; do
     # confirm that file exists; fixes issue when no files are present
     if [[ -f "$file" ]]; then
-
         # skip temporary Word files: files that start with ~$
         if [[ "$file" == '~$'* ]]; then
-            echo " - Skipping the file '${file}' because it starts with '~$'"
+            #echo " - Skipping the file '${file}' because it starts with '~$'"
             continue
         fi
 
@@ -42,7 +43,7 @@ for file in *.docx; do
         
         # create directory and unzip file
         # Important: Use quotes to support file names that contain white space!
-        echo " - Processing file: '${file}'; name: '${name}'"
+        #echo " - Processing file: '${file}'; name: '${name}'"
         mkdir -p "${TEMP_DIR}/${name}"
         cd "${TEMP_DIR}/${name}"
         unzip -qq "../../${file}"
