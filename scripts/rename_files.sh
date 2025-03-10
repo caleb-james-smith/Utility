13fb538e5003ac50bbf46114f2173ca3d912332a
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-03-10 09:20:29 -0500

    Add prefix to files.

--- scripts/rename_files.sh
+++ scripts/rename_files.sh
@@ -9,6 +9,7 @@
 
 INPUT_DIR=$1
 EXTENSION=$2
+PREFIX="OLL_"
 
 # Check if INPUT_DIR is empty
 if [[ -z "$INPUT_DIR" ]]; then
@@ -31,12 +32,23 @@ if [[ ! -d "$INPUT_DIR" ]]; then
     exit 1
 fi
 
+echo "Input directory: ${INPUT_DIR}"
+echo "File extension: ${EXTENSION}"
+echo "Prefix: ${PREFIX}"
+
+# Go to directory
+cd ${INPUT_DIR}
+
 # Loop over files with extension in directory
-for file in ${INPUT_DIR}/*.${EXTENSION}; do
+for file in *.${EXTENSION}; do
     # Confirm that file exists; fixes issue when no files are present.
     if [[ -f "$file" ]]; then
-        echo " - ${file}"
+        echo " - Moving ${file} to ${PREFIX}${file}"
+        mv ${file} ${PREFIX}${file}
     fi
 done
 
+cd ..
+
+echo "Done!"
 
