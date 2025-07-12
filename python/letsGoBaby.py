c7d2d9713cfdb1404115a1baf8f88d588a489eeb
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-07-12 12:08:02 -0500

    Create functions to get conception and due dates.

--- python/letsGoBaby.py
+++ python/letsGoBaby.py
@@ -2,14 +2,25 @@
 
 import datetime
 
-def main():
-    print("Hello, baby!")
+def runBabyCalc():
+    print("Let's go, baby!")
     date_format = "%Y-%m-%d"
+    conception_date_object = getConceptionDateObject(date_format)
+    due_date_object = calcDueDateObject(conception_date_object)
+    due_date = due_date_object.strftime(date_format)
+    print(f"Due date: {due_date}")
+
+def getConceptionDateObject(date_format):
     conception_date = input("Please enter the date of conception (YYYY-MM-DD): ")
     conception_date_object = datetime.datetime.strptime(conception_date, date_format)
+    return conception_date_object
+
+def calcDueDateObject(conception_date_object):
     due_date_object = conception_date_object + datetime.timedelta(weeks=38)
-    due_date = due_date_object.strftime("%Y-%m-%d")
-    print(f"Due date: {due_date}")
+    return due_date_object
+
+def main():
+    runBabyCalc()
 
 if __name__ == "__main__":
     main()
