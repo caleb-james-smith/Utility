f3bff14d4e4aaf38ccfd0eda75137c357a88fbd5
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-11-26 17:08:08 -0600

    Create function to get modification year of file.

--- python/organizeFilesByYear.py
+++ python/organizeFilesByYear.py
@@ -2,11 +2,6 @@ import argparse
 import sys
 import tools
 
-def organizeFilesByYear(directory):
-    files = tools.getFilesInDir(directory)
-    n_files = len(files)
-    print(f"n_files: {n_files}")
-
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--directory", "-d", default="", help="Directory containing files to organize")
@@ -18,6 +13,16 @@ def main():
         sys.exit(1)
     
     organizeFilesByYear(directory)
+
+def organizeFilesByYear(directory):
+    directory = tools.appendSlash(directory)
+    files = tools.getFilesInDir(directory)
+    n_files = len(files)
+    print(f"n_files: {n_files}")
+    print(files)
+    for file in files:
+        path = directory + file
+        year = tools.getModificationYear(path)
 
 if __name__ == "__main__":
     main()
--- python/tools.py
+++ python/tools.py
@@ -1,5 +1,7 @@
 import os
+import sys
 from pathlib import Path
+from datetime import datetime
 
 # creates directory if it does not exist
 def makeDir(dir_name):
@@ -10,4 +12,24 @@ def makeDir(dir_name):
 def getFilesInDir(dir_name):
     directory = Path(dir_name)
     files = [f.name for f in directory.iterdir() if f.is_file()]
+    files.sort()
     return files
+
+# get the modification year of file
+def getModificationYear(file_name):
+    try:
+        time_stamp = os.path.getmtime(file_name)
+        time_stamp_object = datetime.fromtimestamp(time_stamp)
+        year = time_stamp_object.year
+        print(f"file_name: {file_name}, time_stamp: {time_stamp:.2f}, year: {year}")
+        return year
+    except FileNotFoundError:
+        print(f"ERROR: Cannot get time stamp for the file '{file_name}' as it was not found.")
+        sys.exit(1)
+
+# append slash to path if path does not end in slash
+def appendSlash(path):
+    slash = "/"
+    if path[-1] != slash:
+        path += slash
+    return path
